49e4a2f4b30d3f27a7079beb03f25fd3ba0067c7
pce <patrick@madhat.de>
2023-11-17 14:41:02 +0100

    Initial Commit

---
+++ recdr/recdrApp.swift
@@ -0,0 +1,17 @@
+//
+//  recdrApp.swift
+//  recdr
+//
+//  Created by Patrick on 17.11.23.
+//
+
+import SwiftUI
+
+@main
+struct recdrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
